5c61e5d7c8c7bcdc2a8a4d4e10ef8eda33f245a9
Khanyisa-AI <98459205+Khanyisa-AI@users.noreply.github.com>
2022-02-14 10:57:18 +0200

    Update Task05.js

--- Task05.js
+++ Task05.js
@@ -1,6 +1,6 @@
 
 function triangleArea(side1,side2,side3){
-    let semiParameter = (side1+side2+side3)/2;
-    let area = (semiParameter*(semiParameter-side1)*(semiParameter-side2)*(semiParameter-side3))**(0.5);
+    const semiParameter = (side1+side2+side3)/2;
+    const area = (semiParameter*(semiParameter-side1)*(semiParameter-side2)*(semiParameter-side3))**(0.5);
     return area;
 }
