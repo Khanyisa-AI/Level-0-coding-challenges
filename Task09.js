97ea1b19503f1a913e094f5ece1d2af37cf6d5f1
Khanyisa-AI <98459205+Khanyisa-AI@users.noreply.github.com>
2022-02-14 11:09:32 +0200

    Update Task09.js

--- Task09.js
+++ Task09.js
@@ -1,19 +1,19 @@
 function presentVowels(aString)
 
     {//Changing all charecters to lowercase
-    let vowels =[]
+    var vowels =[]
     const vowelCheck = "aeiou"
     aString= aString.toLowerCase()
 
     //Finding present vowels in string
     for (let i = 0; i < aString.length; i++) 
         {for (let j = 0; j < 5; j++) 
-            {if (aString[i]==vowelCheck[j])
+            {if (aString[i]===vowelCheck[j])
                 {vowels.push(aString[i])}
             }; 
         }
 
-    let nonRepeatingVowels = [...new Set(vowels)];//removing reapiting vowels from list of present vowels
-    let output = nonRepeatingVowels.join(", ")
-    return "Vowels: " +output +".";
+    const nonRepeatingVowels = [...new Set(vowels)];//removing reapiting vowels from list of present vowels
+    const output = nonRepeatingVowels.join(", ")
+    console.log("Vowels: " +output +".")
     }
